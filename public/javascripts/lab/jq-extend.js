cf05b1b746e26bc9cc24ca1da5e2b5bb518314dd
ch51f <371476909@qq.com>
2014-07-24 17:53:54 +0800

    update
    增加控制文本框游标

--- public/javascripts/lab/jq-extend.js
+++ public/javascripts/lab/jq-extend.js
@@ -16,4 +16,26 @@ define(["jquery"], function($) {
 			return obj.currentStyle ? obj.currentStyle[styleName] : document.defaultView.getComputedStyle(obj, null)[styleName];
 		}
 	});
+	$.fn.setSelecttion = function (selectionStart, selectionEnd) {
+		if(this.length == 0) return this;
+		input = this[0];
+		if(input.createTextRange) {
+			var range = input.createTextRange();
+			range.collapse(true);
+			range.moveEnd("character", selectionEnd);
+			range.moveStart("character", selectionStart);
+			range.select();
+		} else if (input.setSelectionRange) {
+			input.focus();
+			input.setSelectionRange(selectionStart, selectionEnd);
+		}
+		return this;
+	}
+	$.fn.setCursorPosition = function (position) {
+		if(this.length == 0) return this;
+		return $(this).setSelecttion(position, position);
+	}
+	$.fn.focusEnd = function() {
+		this.setCursorPosition(this.val().length);
+	}
 });
